2661b0f4627668a044ee225820b6603f8cd66777
Purushothaman-source <Purushoth99jun@gmail.com>
2021-01-08 19:58:46 +0530

    DELETED : removed line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package com.git.HelloWorld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-                System.out.println("ADDED Addition  branch in local ");
+                System.out.println("refractored line ");
 	}
 }
